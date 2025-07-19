98a29211a17b2ec6c39892d32a392aa2b256c537
Mosad Ahmed <mosad@example.com>
2025-07-19 16:17:06 +0300

    ????? breakpoints ?? Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,pug}"],
-  theme: {
+content: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx,pug}"]
+,
+theme: {
     extend: {},
   },
   plugins: [],
